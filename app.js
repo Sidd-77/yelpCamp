6da49dd7b53f6d42db812588f76c7ccdc4159791
Sidd-77 <siddharthsalunkhe2003@gmail.com>
2023-06-24 18:25:10 +0530

    sfffirst

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+const db = mongoose.connection;
+db.on("error",console.error.bind(console, "connection error"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
