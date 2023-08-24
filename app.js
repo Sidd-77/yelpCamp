b1b5f4a461081d94c61d9ae962f8f0262a3af223
sid <siddharthsalunkhe2003@gmail.com>
2023-08-25 00:06:21 +0530

    minor changes 1

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Server Port 3000");
 })
